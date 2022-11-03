4a9bb558a4db921934984b2a9e6aeed24738af0b
Alexey Zuev <alexzuza@yandex.ru>
2022-11-03 12:22:11 +0300

    fix: return full error message from failed push

--- libgit2patchedfiles/examples/push.c
+++ libgit2patchedfiles/examples/push.c
@@ -18,6 +18,7 @@ struct push_opts {
 	char* refspec;
 };
 static int push_update_reference_callback(const char* refname, const char* status, void* data);
+static int sideband_progress_callback(const char* str, int len, void* payload);
 static void parse_opts(struct push_opts* o, int argc, char* argv[]);
 
 static void print_usage(void)
@@ -60,6 +61,7 @@ int lg2_push(git_repository* repo, int argc, char** argv) {
 
 	check_lg2(git_push_options_init(&options, GIT_PUSH_OPTIONS_VERSION), "Error initializing push", NULL);
 	options.callbacks.push_update_reference = push_update_reference_callback;
+	options.callbacks.sideband_progress = sideband_progress_callback;
 
 	check_lg2(git_remote_push(remote, &refspecs, &options), "Error pushing", NULL);
 	printf("Successfully pushed to %s\n", refspec);
@@ -67,6 +69,17 @@ int lg2_push(git_repository* repo, int argc, char** argv) {
 	git_reference_free(head_ref);
 
 	return 0;
+}
+
+
+static int sideband_progress_callback(const char* str, int len, void* payload)
+{
+    if (strstr(str, "ERR") || strstr(str, "403"))
+    {
+	    fprintf(stderr, "%s\n", str);
+    }
+    
+    return 0;
 }
 
 static int push_update_reference_callback(const char* refname, const char* status, void* data)
