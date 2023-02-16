950038ea2332eb186b7671940a4fb7e6271d3ab4
Alexey Zuev <alexzuza@yandex.ru>
2023-02-16 13:02:50 +0300

    fix: properly revert merge commits

--- libgit2patchedfiles/examples/revert.c
+++ libgit2patchedfiles/examples/revert.c
@@ -4,6 +4,7 @@ int lg2_revert(git_repository* repo, int argc, char** argv)
 {
 	git_commit* target_commit = NULL;
 	git_revspec revspec;
+	git_revert_options opts = GIT_REVERT_OPTIONS_INIT;
 	int err;
 
 	err = git_revparse(&revspec, repo, argv[argc - 1]);
@@ -19,7 +20,15 @@ int lg2_revert(git_repository* repo, int argc, char** argv)
 		goto cleanup;
 	}
 
-	err = git_revert(repo, target_commit, NULL);
+	if (argc > 2)
+	{
+		if (!strcmp(argv[argc - 2], "-m"))
+		{
+			opts.mainline = 1;
+		}
+	}
+
+	err = git_revert(repo, target_commit, &opts);
 	if (err != 0)
 	{
 		fprintf(stderr, "revert error: %s\n", git_error_last()->message);
